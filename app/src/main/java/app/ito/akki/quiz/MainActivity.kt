948914228dfda7e3828911d622413434551a2250
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-02-13 17:53:11 +0900

    Initial commit

---
+++ app/src/main/java/app/ito/akki/quiz/MainActivity.kt
@@ -0,0 +1,21 @@
+package app.ito.akki.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //クイズ画面に移動する準備をする
+        val quizIntent: Intent = Intent(this,QuizActivity::class.java)
+
+        startButton.setOnClickListener {
+            //クイズ画面に移動する
+            startActivity(quizIntent)
+        }
+    }
+}
